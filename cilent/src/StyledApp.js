d05f2b9d599fe0a02d707a25823f359b93bd2ffe
Dongyue Zhang <zhangdongyue22@gmail.com>
2022-09-03 18:21:43 -0400

    hard coded url

--- cilent/src/StyledApp.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const AppContainer = styled.div`
-    margin: 1rem;
-    font-family:Arial, Helvetica, sans-serif;
-    color: #222222;
-`;
--- cilent/src/components/selectors.js
+++
@@ -1,9 +0,0 @@
-
-export const getRecords = state => {
-    return state.records.data.map((record) => {
-        record.date = new Date(record.date);
-        return record;
-    })
-};
-export const getRecordsLoading = state => state.records.isLoading;
-
